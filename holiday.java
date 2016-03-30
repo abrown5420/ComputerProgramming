14ac502360b1392d7ed38627444b109aed036807
abrown5420 <totalltony@hotmail.com>
2016-03-30 11:23:28 -0500

    Added files via upload

---
+++ holiday.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson2;
+
+/**
+ *
+ * @author Antho_brown986
+ */
+public class holiday {
+    public static void main(String args[])
+    {
+        String name,a,b,c,d;
+        name= "Favorite holidays for Anthony";
+        a = "Christmas in December";
+        b = "Thanksgiving in November";
+        c = "Easter in March";
+        d= "New Years in January";
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson2;
+
+/**
+ *
+ * @author Antho_brown986
+ */
+public class variable 
+{
+    public static void main(String args[])
+    {
+        double payRate = 35.87;
+        int a,b,c;
+        a = 22;
+        b = 4;
+        c = 88;
+        System.out.println(payRate);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
